ea06e701c9e0a540a0e384119cb0b033be0b6ad6
Sunitha1809 <23CSB52@karpagamtech.ac.in>
2026-02-26 09:13:24 +0530

    Create login_page.py

---
+++ login_page.py
@@ -0,0 +1,27 @@
+from selenium.webdriver.common.by import By
+
+
+class LoginPage:
+
+    def __init__(self, driver):
+        self.driver = driver
+
+        self.username_id = "user-name"
+        self.password_id = "password"
+        self.login_button_id = "login-button"
+
+    def enter_username(self, username):
+        self.driver.find_element(By.ID, self.username_id).clear()
+        self.driver.find_element(By.ID, self.username_id).send_keys(username)
+
+    def enter_password(self, password):
+        self.driver.find_element(By.ID, self.password_id).clear()
+        self.driver.find_element(By.ID, self.password_id).send_keys(password)
+
+    def click_login(self):
+        self.driver.find_element(By.ID, self.login_button_id).click()
+
+    def login(self, username, password):
+        self.enter_username(username)
+        self.enter_password(password)
+        self.click_login()
